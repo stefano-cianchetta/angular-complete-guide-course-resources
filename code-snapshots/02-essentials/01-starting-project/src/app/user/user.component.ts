6f41d71b529d4043367a2a4be4857b2e1a66750c
stefano <stefano.cianchetta@gmail.com>
2025-06-15 15:56:49 +0200

    tasks component

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,7 +13,9 @@ export class UserComponent {
   @Input({ required: true }) name!: string;
 
   // custom select event
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
+  userName = output<string>();
 
   // avatar = input.required<string>();
   // name = input.required<string>();
@@ -28,5 +30,6 @@ export class UserComponent {
 
   onSelectUser() {
     this.select.emit(this.id);
+    this.userName.emit(this.name);
   }
 }
