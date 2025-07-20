f8c4aef3e406404c81d547ed8195f6c60026f2e1
stefano <stefano.cianchetta@gmail.com>
2025-07-20 16:26:45 +0200

    ng-content + services

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 // type User = {
 //   id: string;
@@ -17,7 +18,7 @@ import { User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
