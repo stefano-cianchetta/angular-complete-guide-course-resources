f6b22c75599c5e525730672a39e77d26d6ac8ef4
stefano <stefano.cianchetta@gmail.com>
2025-10-12 01:40:01 +0200

    modules

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.service.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.service.ts
@@ -29,21 +29,36 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
   addTasks(taskData: NewTaskData, userId: string) {
-    return this.tasks.unshift({
+    this.tasks.unshift({
       id: new Date().getTime().toString(),
       userId: userId,
       title: taskData.title,
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
