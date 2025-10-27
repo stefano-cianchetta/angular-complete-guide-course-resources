7974825e7821013beccd1bf6797dbb9a3d7c6a73
stefano <stefano.cianchetta@gmail.com>
2025-10-27 02:31:39 +0100

    host element binding - css class binding

--- code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
+++ code-snapshots/06-cmp-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,7 +14,22 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log(`Clicked!`);
+    console.log(this.el);
+  }
 }
